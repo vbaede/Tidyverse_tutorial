6beca33dfc9797c5804d93d8e26675cc3bd59a2b
Valérie Baede <vbaede@gmail.com>
2022-10-05 17:00:30 +0200

    Update tutorial

--- Data_visualization.R
+++ Data_visualization.R
@@ -1,7 +1,11 @@
 ##Data visualization
 
-library(tidyverse)
+# Tidyverse training
+# Following the R for Data Science book to learn the Tidyverse at https://www.tidyverse.org/
+# https://r4ds.had.co.nz/
 
+#Libraries
+library(tidyverse)
 install.packages(c("nycflights13", "gapminder", "Lahman"))
 
 #3.2
@@ -27,3 +31,42 @@ ggplot(mpg) +
 #3.3
 ggplot(mpg) + 
   geom_point(aes(displ, hwy, colour = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, size = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))
+
+# Once you map an aesthetic, ggplot2 takes care of the rest. It selects a reasonable scale to use with the aesthetic, 
+# and it constructs a legend that explains the mapping between levels and values. For x and y aesthetics, ggplot2 does not create a legend, 
+# but it creates an axis line with tick marks and a label. The axis line acts as a legend; it explains the mapping between locations and values.
+
+# so only geom_levels are given in legend. aesthetics on main level are given on axis
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
+# To set an aesthetic manually, set the aesthetic by name as an argument of your geom function; i.e. it goes outside of aes().
+
+#3.3.1 exercises
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = "blue")) # color should be outside of aes
+str(mpg) # see variable classes to determine categorical and continuous variables
+
+ggplot(mpg, aes(x = displ, y = hwy, color = cty)) + # colour gradient
+  geom_point() 
+ggplot(mpg, aes(x = displ, y = hwy, size = cty)) + # size gradient
+  geom_point()
+ggplot(mpg, aes(x = displ, y = hwy, shape = cty)) + # continuous variable cannot be mapped to a shape
+  geom_point()
+ggplot(mpg, aes(x = displ, y = hwy, color = cty, size = cty)) + # both aesthetics shown
+  geom_point()
+
+ggplot(mpg, aes(x = displ, y = hwy)) + 
+  geom_point(aes(stroke=cty)) 
+ggplot(mtcars, aes(wt, mpg)) +
+  geom_point(shape = 21, colour = "black", fill = "white", size = 5, stroke = 5)
+ggplot(mpg, aes(x = displ, y = hwy)) + # specify colour aesthetics to values of displ < 5
+  geom_point(aes(colour = displ < 5)) 
+
+#3.5 Facets
--- Wrangle.R
+++ Wrangle.R
@@ -1,5 +1,10 @@
 ##Wrangle
 
+# Tidyverse training
+# Following the R for Data Science book to learn the Tidyverse at https://www.tidyverse.org/
+# https://r4ds.had.co.nz/
+
+#Libraries
 library(tidyverse)
 
 #10.2
@@ -41,11 +46,42 @@ df
 df$x # show values in column x
 df[["x"]] # show values in column x
 df[[1]] # show values in first column
+df%>% .$x
+df%>% .[["x"]]
 
 #10.5
 mtcars
 
 df <- data.frame(abc = 1, xyz = "a")
-df$x
-df[, "xyz"]
-df[, c("abc", "xyz")]
+df
+df$x # on a tibble this will throw a warning. Partial matching is not allowed
+df[, "xyz"] # on a tibble this will be a data frame still
+df[, c("abc", "xyz")] # This will be the same result in a tibble
+
+tb <- tibble( # for non-syntactic column names, use backticks `...` also in other tidyverse packages like ggplot
+  `abc` = 1, 
+  `xyz` = "a"
+)
+tb$xyz
+tb[, "xyz"]
+tb[, c("abc", "xyz")]
+
+# var <- "mpg"
+# as_tibble(mtcars)[[var]]
+mpg[["manufacturer"]]
+mpg[["model"]]
+
+annoying <- tibble(
+  `1` = 1:10,
+  `2` = `1` * 2 + rnorm(length(`1`))
+)
+annoying
+annoying[["1"]]
+ggplot(annoying, aes(`1`, `2`)) + #backticks `` gebruiken
+  geom_point()
+
+annoying <- tibble(
+  `1` = 1:10,
+  `2` = `1` * 2 + rnorm(length(`1`)),
+  `3` = `2`/`1`
+)
